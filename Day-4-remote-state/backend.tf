c755604779a6f6a6b2ce3a90aa5d0f160b325e50
deepak <kdeepak.cd@gmail.com>
2026-01-05 01:08:45 +0530

    Backend-lock

--- Day-4-remote-state/backend.tf
+++ Day-4-remote-state/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "test-bucket-hyd-bns-backend"
     key    = "test/terraform.tfstate"
     region = "us-east-1"
+    use_lockfile   = true # Enable S3 native locking
+    # Remove the 'dynamodb_table' parameter if migrating from the legacy method
   }
 }
