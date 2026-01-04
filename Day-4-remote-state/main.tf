17dc926dc04fb637064e97a28586ae3ecdb6a6a5
deepak <kdeepak.cd@gmail.com>
2026-01-04 22:57:29 +0530

    day-4-Updated

--- Day-4-remote-state/main.tf
+++ Day-4-remote-state/main.tf
@@ -1,7 +1,14 @@
 resource "aws_instance" "test" {
-    ami = "ami-068c0051b15cdb816"
-    instance_type = "t3.micro"
+  ami           = "ami-068c0051b15cdb816"
+  instance_type = "t3.micro"
+
   tags = {
     Name = "test-server"
   }
+  
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "test-bucket-hyd-bns-backend"
+  
 }
