8f1a6a76290e49e9f14ef63d2c70f1e10ced4c67
deepak <kdeepak.cd@gmail.com>
2026-01-07 13:05:32 +0530

    Lambda-uploadcode

---
+++ Day-7-Lambda-upload-code-local/main.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_policy" "lambda_exec_policy" {
+    name        = "lambda_exec_policy"
+    description = "IAM policy for Lambda execution role"
+    policy      = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = [
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"
+                ]
+                Effect   = "Allow"
+                Resource = "arn:aws:logs:*:*:*"
+            }
+        ]
+    })
+  
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+    name               = "lambda_exec_role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+            }
+        ]
+    })
+  
+}
+
+resource "aws_iam_role_policy_attachment" "attach_lambda_exec_policy" {
+    role       = aws_iam_role.lambda_exec_role.name
+    policy_arn = aws_iam_policy.lambda_exec_policy.arn
+}
+
+resource "aws_lambda_function" "my_lambda" {
+    function_name = "my_lambda_function"
+    role          = aws_iam_role.lambda_exec_role.arn
+    handler       = "function_test.lambda_handler"
+    runtime       = "python3.14"
+    filename      = "function_test.zip"
+    source_code_hash = filebase64sha256("function_test.zip")
+    timeout = 300
+    memory_size = 128
+  
+}
