c514823a6c0b27b109f457586e7bac99cfdc2c6b
deepak <kdeepak.cd@gmail.com>
2026-01-07 13:11:56 +0530

    Update

--- Day-7-Lambda-upload-code-local/main.tf
+++ Day-7-Lambda-upload-code-local/main.tf
@@ -1,3 +1,4 @@
+# IAM Policy for Lambda Execution Role
 resource "aws_iam_policy" "lambda_exec_policy" {
     name        = "lambda_exec_policy"
     description = "IAM policy for Lambda execution role"
@@ -18,6 +19,7 @@ resource "aws_iam_policy" "lambda_exec_policy" {
   
 }
 
+# IAM Role for Lambda Execution
 resource "aws_iam_role" "lambda_exec_role" {
     name               = "lambda_exec_role"
     assume_role_policy = jsonencode({
@@ -35,18 +37,26 @@ resource "aws_iam_role" "lambda_exec_role" {
   
 }
 
+# Attach the policy to the role
 resource "aws_iam_role_policy_attachment" "attach_lambda_exec_policy" {
     role       = aws_iam_role.lambda_exec_role.name
     policy_arn = aws_iam_policy.lambda_exec_policy.arn
 }
 
+# Create Lambda Function
 resource "aws_lambda_function" "my_lambda" {
     function_name = "my_lambda_function"
     role          = aws_iam_role.lambda_exec_role.arn
+
+    # Specify the handler, runtime, and the local zip file
     handler       = "function_test.lambda_handler"
     runtime       = "python3.14"
     filename      = "function_test.zip"
+
+    #For local file upload, we need to provide source_code_hash
     source_code_hash = filebase64sha256("function_test.zip")
+
+    # Configure timeout and memory size
     timeout = 300
     memory_size = 128
   
